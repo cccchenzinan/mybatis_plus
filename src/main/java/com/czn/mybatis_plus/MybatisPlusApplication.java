716cd122ce747fd8b844e1a8a5c1d0ea0b02d6d0
cccchenzinan <kukuchenzinan@outlook.com>
2019-07-31 16:11:25 +0800

    change test

--- src/main/java/com/czn/mybatis_plus/MybatisPlusApplication.java
+++ src/main/java/com/czn/mybatis_plus/MybatisPlusApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MybatisPlusApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(MybatisPlusApplication.class, args);
-    }
+    public static void main(String[] args) { SpringApplication.run(MybatisPlusApplication.class, args);}
 
 }
